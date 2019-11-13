2e72a1e29cf0491a9a2e4ec1d59312ccade2456e
letuananh <lepacco19944@gmail.com>
2019-11-13 10:04:19 +0700

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import '../styles/App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div style={{textAlign: 'center'}}>
+                <h1>My React App - BadCuder!</h1>
+                <img src="https://avatars3.githubusercontent.com/u/48653662?s=460&v=4" />
+            </div>
+        );
+    }
+}
+
+export default App;
